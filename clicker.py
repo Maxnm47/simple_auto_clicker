9c029d05f87d637ce91df183053ae45e5a810b62
Max Møller <96955526+Maxnm47@users.noreply.github.com>
2023-04-26 12:40:35 +0200

    Add files via upload

---
+++ clicker.py
@@ -0,0 +1,25 @@
+import pyautogui
+import keyboard
+
+#finder pos:
+def pos_finder():
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+        if keyboard.is_pressed('enter'):
+            break
+    return (x, y)
+pos = pos_finder()
+
+x = pos[0]
+y = pos[1]
+
+pyautogui.moveTo(pos)
+while pos == (x,y):
+    print("test")
+    x, y = pyautogui.position()
+    pyautogui.click(x, y)
+
+
